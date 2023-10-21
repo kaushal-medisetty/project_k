0a74ee254f9359b8c78c15096035e78dbec2675f
unknown <225003203@sastra.ac.in>
2023-10-21 21:35:41 +0530

    sddsd

--- src/mongo_connect.js
+++ src/mongo_connect.js
@@ -20,23 +20,63 @@ const reportschema = new mongoose.Schema({
   email: String,
   desc: String,
   report_name: String,
+  dest: String,
   data: Date,
 });
 
 const reports = mongoose.model("report", reportschema);
 
+
+const alertschema = new mongoose.Schema({
+  Drug: String,
+  email: String,
+  desc: String,
+  report_time: String,
+  data: Date,
+});
+
+
+const alert = mongoose.model("alert",alertschema);
+
+
+
+exports.up_alert = async (u_email,a_drug,a_desc,a_time,a_date) => {
+  //(username , user_email, report name , report Desc)
+  const N_alert = {
+    Drug: a_drug,
+    email:u_email,
+    desc: a_desc,
+    report_time: a_time,
+    data: a_date,
+  };
+  let ralert = new alert(N_alert);
+  await ralert.save();
+  return true;
+};
+
+exports.getalert = async () => {
+  const alert1 = await alert.find({  });
+  return alert1;
+};
+
+exports.alert_del = async (id) => { alert.deleteOne({_id:id}).then(function () {
+  console.log("done");
+})
+}
 exports.getreport = async (u_email) => {
   const report = await reports.find({ email: u_email });
   return report;
 };
 
-exports.up_reports = async (u_name, u_email, rep_name, rep_des) => {
+exports.up_reports = async (u_email,u_name,rep_des, rep_name,rep_dest ) => {
   //(username , user_email, report name , report Desc)
   const N_report = {
     email: u_email,
     name: u_name,
     desc: rep_des,
     report_name: rep_name,
+    //data: Date1,
+    dest:rep_dest,
     data: Date.now(),
   };
   let rport = new reports(N_report);
@@ -50,7 +90,7 @@ exports.signup = async (data) => {
 };
 
 exports.checkauth = async (auth1) => {
-  console.log("check1:" + auth1);
+ 
   let credentials = jwt.dectoks(auth1);
   console.log(
     ">> DecToks <<< :" + credentials.name + "<<<" + credentials.email
@@ -66,6 +106,16 @@ exports.checkauth = async (auth1) => {
     return usersdata;
   }
 };
+
+
+exports.reports_del = async (id) => { reports.deleteOne({_id:id}).then(function () {
+  console.log("done");
+})
+.catch(function (err) {
+  console.log(err);
+});
+}
+
 
 exports.signin = async (data1) => {
   try {
