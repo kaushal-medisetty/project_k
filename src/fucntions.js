9619319bf69df580817d5c24c13350e0107a9832
MonkNo1 <hitman20025@gmail.com>
2023-10-24 00:54:14 +0530

    app date function

---
+++ src/fucntions.js
@@ -0,0 +1,8 @@
+exports.makedataTime = (data, time) => {
+  date = date.toString();
+  time = time.toString();
+  date = date.split("T");
+  let temp = date[1].split(":");
+  let newdate = date[0] + "T" + time + ":" + temp[2] + ":" + temp[3];
+  console.log(newdate);
+};
